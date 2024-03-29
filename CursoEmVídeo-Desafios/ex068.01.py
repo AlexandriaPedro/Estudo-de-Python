eaafdaf4974ccaacaacd1ec01e94384a9233117a
alexandria <pedro.alexandria@hop.digital>
2024-03-29 19:06:19 -0300

    exercise 68 and 69 done

--- CursoEmVídeo-Desafios/ex068.01.py
+++ CursoEmVídeo-Desafios/ex068.01.py
@@ -1,3 +1,56 @@
 #DESAFIO - 068
 #Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o
 #jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+import random
+
+print('-='*51)
+print(f'{" Í M P A R  ou  P A R ":~^102}')
+print('-='*51)
+
+vitorias = soma = 0
+comp = ''
+
+while True:
+    resposta1 = str(input('escolha entre ímpar ou par: '))
+    resposta1 = resposta1.capitalize().strip()
+
+    if resposta1 == 'Impar' or resposta1 == 'Ímpar':
+        comp = 'Par'
+    if resposta1 == 'Par' :
+        comp = 'Ímpar'
+    
+    respcomp = random.randint(0, 10)
+
+    resposta2 = int(input('Diga lá! Qual número vai jogar? '))
+    print("\n")
+
+    soma = respcomp + resposta2
+
+    if (soma % 2 == 0) and (comp == "Par"):
+        print(f"Como o computador escolheu o número {respcomp}, então a soma deu {soma} e o computador venceu, porque {soma} é Par!")
+        print("Tente de novo, depois!")
+        print("\n")
+        print(f"Saldo de vitórias: {vitorias}")
+        print('-='*51)
+        break
+    elif (soma % 2 == 0) and (comp == "Ímpar"):
+        print(f"Como o computador escolheu o número {respcomp}, então a soma deu {soma} e o Você venceu, porque {soma} é Par!")
+        print("Muito Massa!")
+        print("\n")
+        print("Bora pra próxima :p")
+        print('-='*51)
+        vitorias += 1
+    elif (soma % 2 != 0) and (comp == "Ímpar"):
+        print(f"Como o computador escolheu o número {respcomp}, então a soma deu {soma} e o computador venceu, porque {soma} é ímpar!")
+        print("Tente de novo, depois!")
+        print("\n")
+        print(f"Saldo de vitórias: {vitorias}")
+        print('-='*51)
+        break
+    elif (soma % 2 != 0) and (comp == "Par"):
+        print(f"Como o computador escolheu o número {respcomp}, então a soma deu {soma} e o Você venceu, porque {soma} é Ímpar!")
+        print("Muito Massa!")
+        print("\n")
+        print("Bora pra próxima :p")
+        print('-='*51)
+        vitorias += 1
--- CursoEmVídeo-Desafios/ex069.01.py
+++ CursoEmVídeo-Desafios/ex069.01.py
@@ -2,3 +2,43 @@
 #Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa
 #deverá perguntar se o usuário quer ou não continuar. No final, mostre: A) Quantas pessoas tem mais
 #de 18 anos; B) Quantos homens foram cadastrados; C) Quantas mulheres tem menos de 20 anos.
+total_mais_18 = quant_h = quant_m_menos_20 = 0
+
+while True:
+    while True:
+        sexo = str(input("Diga lá, seu sexo: (M ou H) "))
+        sexo = sexo.capitalize().strip()
+
+        if sexo != 'M' and sexo != 'H':
+            print("Dê um dos dois, meu fi. Só pode ser ou M ou H, então bora!")
+            print("\n")
+        else:
+            break
+    
+    idade = int(input("Diga lá agora, qual sua idade, meu fi? "))
+
+    if idade > 18:
+        total_mais_18 += 1
+    if sexo == 'H':
+        quant_h += 1
+    if sexo == 'M' and idade < 20:
+        quant_m_menos_20 += 1
+
+    print("\n")
+
+    while True:
+        resp = str(input("Quer cadastrar mais alguem? (S ou N)"))
+        resp = resp.capitalize().strip()
+        
+        if resp != 'S' and resp != 'N':
+            print("Dê um dos dois, meu fi. Só pode ser ou M ou H, então bora!")
+            print("\n")
+        else:
+            break
+    
+    if resp == 'N':
+        print("Pois bem, então te mostrarei os resultados até então...")
+        print(f"Quantidade de pessoas maiores de 18 anos: {total_mais_18}")
+        print(f"Quantidade total de homens : {quant_h}")
+        print(f"Quantidade de mulheres com menos de 20 anos: {quant_m_menos_20}")
+        break
