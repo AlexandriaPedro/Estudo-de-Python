63b8cf7cad48aa01f1fdd289f0c447bd6fa2570c
alexandria <pedro.alexandria@hop.digital>
2024-03-29 22:02:52 -0300

    exercise 70 and 71 done

--- CursoEmVídeo-Desafios/ex070.01.py
+++ CursoEmVídeo-Desafios/ex070.01.py
@@ -2,3 +2,44 @@
 #Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar
 #se o usuário vai continuar. No final, mostre: A) Qual é o total gasto na compra;
 #B) Quantos produtos custam mais de R$1000; C) Qual é o nome do produto mais barato.
+total_gasto = quanto_mais_de_1000 = preco_produto_barato = contador = 0
+qual_mais_barato = ''
+
+while True:
+    nome_produto = str(input("Me fale o nome do produto: "))
+    preco_produto = float(input("Me fale o preço do produto: "))
+
+    contador += 1
+
+    total_gasto += preco_produto
+
+    if preco_produto > 1000:
+        quanto_mais_de_1000 += 1
+    
+    if contador == 1:
+        qual_mais_barato = nome_produto
+        preco_produto_barato = preco_produto
+    elif preco_produto < preco_produto_barato:
+        qual_mais_barato = nome_produto
+        preco_produto_barato = preco_produto
+    
+    while True:
+        resp = str(input("Quer continuar a registrar produtos? (S ou N) "))
+        resp = resp.capitalize().strip()
+
+        if resp != 'N' and resp != 'S':
+            print("Me ajuda ai né, responde algo que existe mlk! É ou S ou N, pô")
+            print("Tenta de novo.")
+            print("\n")
+        else:
+            break
+
+    qual_mais_barato = qual_mais_barato.capitalize().strip()
+    
+    if resp == 'N':
+        print("Beleza, então vamos te dar o resultado da sua compra...")
+        print(f"Bem, o tal gasto foi de {total_gasto:.2f} reais")
+        print(f"{quanto_mais_de_1000} produtos custaram mais de 1000 reais")
+        print(f"E o produto de nome {qual_mais_barato} foi o produto mais barato que você comprou,")
+        print(f"valendo apenas {preco_produto_barato:.2f} reais.")
+        break
--- CursoEmVídeo-Desafios/ex071.01.py
+++ CursoEmVídeo-Desafios/ex071.01.py
@@ -2,3 +2,36 @@
 #Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário
 #qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada
 #valor serão entregues. OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+print("-=" * 52)
+print(f'{"C A I X A   E L E T R Ô N I C O":^104}')
+print("-=" * 52)
+
+quant_50 = quant_20 = quant_10 = quant_1 = 0
+
+valor = int(input("Falaaaa Mickey, quer tirar qual valor do caixa hoje? "))
+
+resto = valor
+
+while True:
+    if resto >= 50:
+        resto -= 50
+        quant_50 += 1
+    if resto >= 20:
+        resto -= 20
+        quant_20 += 1
+    if resto >= 10:
+        resto -= 10
+        quant_10 += 1
+    if resto >= 1:
+        resto -= 1
+        quant_1 += 1
+    if resto == 0:
+        break
+
+print("\n")
+print(f"Dado ao valor que nos foi solicitado, ou seja {valor:.2f} reais,")
+print("Você receberá a seguinte quantidade de notas:")
+print(f"{quant_50} de 50 reais")
+print(f"{quant_20} de 20 reais")
+print(f"{quant_10} de 10 reais")
+print(f"{quant_1} de 1 real")
